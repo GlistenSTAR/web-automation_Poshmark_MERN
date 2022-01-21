5f5a4e5663eaf3f8ff65c7b388da7b0005e6805c
GlistenSTAR <glistenstar00@gmail.com>
2022-01-21 09:03:23 +0100

    add input function firstname, secondname, email, username, password

--- backend/api/users.js
+++ backend/api/users.js
@@ -14,27 +14,38 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @route   POST api/users/register
 // @desc    Register user
 // @access  Public
-router.post('/register',async (req, res) => {
+router.post('/register', async (req, res) => {
     let { first_name, second_name, email, username, password, gender, country } = req.body;
-  
     try{
+        gender = gender
         console.log("starting scraping......")
         let browser = await browserObject.startBrowser();
         let page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);
         // Navigate to the selected page
         await page.goto("https://poshmark.com/signup");
-        // Wait for the required DOM to be rendered
-        await page.waitForSelector('.signup.view');
         // Get the link to all the required books
         await page.type('#firstName', first_name)
         await page.type('#lastName', second_name)
+        await page.type('#email', email)
+        await page.type('#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(3) > input', username)
+        await page.type('#password', password)
+        // await page.$eval("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(1) > div > div", e =>
+        //     e.innerHTML = gender   // This is where the exception occurs
+        // );
+        // await page.evaluate((gender) => {
+        //     document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(1)").className = "dropdown__menu__item"
+        //     gender=="Male"?document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(2)").className = "dropdown__menu__item--selected dropdown__menu__item":null
+        //     gender=="Female"?document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(2)").className = "dropdown__menu__item--selected dropdown__menu__item":null
+        //     gender=="Unspecified"?document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(2)").className = "dropdown__menu__item--selected dropdown__menu__item":null
+        // });
+        // await page.type('#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(1) > div', gender)
 
         await Promise.all([
             page.click('#content > div > div > div.p--v--5 > div.pm-form > form > div.form__actions.signup__footer > button'),
             page.waitForNavigation(),
         ]);
         await page.screenshot({ path: 'example.png' });
-        console.log(urls);
         
     } catch (error) {
         console.error(error)
