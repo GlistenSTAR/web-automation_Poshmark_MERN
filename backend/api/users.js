b2d437e1e9757ce1d3b93a74a2e17780e0e01e7f
GlistenSTAR <glistenstar00@gmail.com>
2022-01-21 17:47:17 +0100

    complete the backend api

--- backend/api/users.js
+++ backend/api/users.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
-const cheerio = require('cheerio');
-const puppeteer = require('puppeteer');
 
 const browserObject = require('../utils/open_browser');
 
@@ -17,38 +14,102 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 router.post('/register', async (req, res) => {
     let { first_name, second_name, email, username, password, gender, country } = req.body;
     try{
-        gender = gender
         console.log("starting scraping......")
         let browser = await browserObject.startBrowser();
         let page = await browser.newPage();
+
         await page.setDefaultNavigationTimeout(0);
-        // Navigate to the selected page
         await page.goto("https://poshmark.com/signup");
-        // Get the link to all the required books
+
         await page.type('#firstName', first_name)
         await page.type('#lastName', second_name)
         await page.type('#email', email)
         await page.type('#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(3) > input', username)
         await page.type('#password', password)
-        // await page.$eval("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(1) > div > div", e =>
-        //     e.innerHTML = gender   // This is where the exception occurs
-        // );
-        // await page.evaluate((gender) => {
-        //     document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(1)").className = "dropdown__menu__item"
-        //     gender=="Male"?document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(2)").className = "dropdown__menu__item--selected dropdown__menu__item":null
-        //     gender=="Female"?document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(2)").className = "dropdown__menu__item--selected dropdown__menu__item":null
-        //     gender=="Unspecified"?document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(2)").className = "dropdown__menu__item--selected dropdown__menu__item":null
-        // });
-        // await page.type('#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(1) > div', gender)
+        
+        if(gender == "ma"){
+            await page.evaluate(() => {
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(1)").className = "dropdown__menu__item"
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(2)").className = "dropdown__menu__item--selected dropdown__menu__item"
+            });
+        }
+        if(gender == "fe"){
+            await page.evaluate(() => {
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(1)").className = "dropdown__menu__item"
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(3)").className = "dropdown__menu__item--selected dropdown__menu__item"
+            })
+        }
+        if(gender == "un"){
+            await page.evaluate(() => {
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(1)").className = "dropdown__menu__item"
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(4)").className = "dropdown__menu__item--selected dropdown__menu__item"
+            })
+        }
+
+        if(country == "ca"){
+            await page.evaluate(() => {
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(1)").className = "dropdown__menu__item"
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(2)").className = "dropdown__menu__item--selected dropdown__menu__item--highlighted dropdown__menu__item"
+            })
+        }
+        if(country == "au"){
+            await page.evaluate(() => {
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(1)").className = "dropdown__menu__item"
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(3)").className = "dropdown__menu__item--selected dropdown__menu__item--highlighted dropdown__menu__item"
+            })
+        }
+        if(country == "in"){
+            await page.evaluate(() => {
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(1)").className = "dropdown__menu__item"
+                document.querySelector("#content > div > div > div.p--v--5 > div.pm-form > form > div:nth-child(5) > div > div:nth-child(2) > ul > li:nth-child(4)").className = "dropdown__menu__item--selected dropdown__menu__item--highlighted dropdown__menu__item"
+            })
+        }
 
         await Promise.all([
             page.click('#content > div > div > div.p--v--5 > div.pm-form > form > div.form__actions.signup__footer > button'),
             page.waitForNavigation(),
-        ]);
-        await page.screenshot({ path: 'example.png' });
-        
+        ]).then(()=>{
+            return {msg : "Successfully registed!"}
+        }).catch((err) => console.log(err));
+
+        await page.close();
+
     } catch (error) {
         console.error(error)
+    }
+});
+
+// @route   POST api/users/getUserInfo
+// @desc    get user infos
+// @access  Public
+router.post('/getUserInfo', async (req, res) =>{
+    let { url } = req.body
+    let data = {}, listing, error
+    try{
+        console.log("starting scraping......")
+        let browser = await browserObject.startBrowser();
+        let page = await browser.newPage();
+
+        await page.setDefaultNavigationTimeout(0);
+        await page.goto(url);
+
+        error = await page.$eval('#content > div > div:nth-child(2) > h1', h1 => h1 )
+        
+        if( error.contains("The page you are looking for could not be found")){
+            return { msg : "The page you are looking for could not be found"}
+        }
+        
+        data["image"] = await page.$eval('#content > div > div.m--b--5 > div > div.ps--r > div.closet__header__info > div > div.d--fl.jc--fe.ai--fe.col-l5.col-x6 > div > img', img => img.src)
+        data["name"] = await page.$eval('#content > div > div.m--b--5 > div > div.ps--r > div.closet__header__info > div > div.col-l19.col-x18 > div > h1 > span.ellipses', span => span.textContent) + " " + await page.$eval('#content > div > div.m--b--5 > div > div.ps--r > div.closet__header__info > div > div.col-l19.col-x18 > div > h1 > span.m--l--2', span => span.textContent)
+        listing = await page.$eval('#content > div > div.m--b--5 > div > div.closet__header__info__user-details__container__full-width > div > div.d--fl.jc--sb.col-x24.col-l19 > nav > ul > li:nth-child(1) > a ', a => a.textContent.trim())
+        data["listing"] = listing.split(" ")[0].trim()
+        
+        resolve(data);
+        await page.close();
+        return { data, msg:"Successfully get user infos"}
+        
+    } catch (err){
+        console.log(err)
     }
 });
 
